753a8f3e6af32846b33f9dfaa6df0d3a5ba1f8c9
Laussel LOïc <loic.laussel@orange.com>
2017-01-24 12:03:24 +0100

    load ressources as relative to current domain

--- serveur/crossdom/js/toolbar.js
+++ serveur/crossdom/js/toolbar.js
@@ -1,8 +1,8 @@
 /* orange-confort-plus - version 4.0.0 - 24-01-2017
 enhance user experience on websites
  Copyright (C) 2014 - 2017 Orange */
-var hebergementDomaine = 'http://confort-plus.orange.com';
-var hebergementFullPath = hebergementDomaine + '/serveur/crossdom/';
+var hebergementDomaine = '';
+var hebergementFullPath = hebergementDomaine + 'serveur/crossdom/';
 // Source: app/conf/hebergement.js
 /*
 
