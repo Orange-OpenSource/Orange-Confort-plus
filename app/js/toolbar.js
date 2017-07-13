29c2e2c2928f74c495fc729246d84db2e451081e
Laussel LOïc <loic.laussel@orange.com>
2017-07-13 10:05:04 +0200

    take into account font-size set on body.
    If body font-size is not the same as html tag, then take the one from body, and set body font-size to 100%
    Try to adress issue #21

--- app/js/toolbar.js
+++ app/js/toolbar.js
@@ -3253,7 +3253,7 @@ accessibilitytoolbar = {
      * 2. add a new STYLE node with the user's preferences
      */
     setCSS: function (init) {   
-        var links, i, allElts,  done, mask, doneMask, imageAlt, spanImage, element, image_uci, s = "", indexFrame, theFrame, theFrameDocument, theFrames, fontSizeDef, toolbarContent;
+        var links, i, allElts,  done, mask, doneMask, imageAlt, spanImage, element, image_uci, s = "", indexFrame, theFrame, theFrameDocument, theFrames, fontSizeDef, toolbarContent, fontSizeBody;
         if (accessibilitytoolbar.userPref.get("a11yToolbarEnable") !== "off") {
             if(document.getElementById('cdu_close'))
             {
@@ -3345,9 +3345,12 @@ accessibilitytoolbar = {
             // generate the CSS instructions
             // 1. do we want bigger fonts?
             // make it proportional to the initial font          
-            fontSizeDef = '16px';
+            fontSizeDef = fontSizeBody = '16px';
             if(window.getComputedStyle) {
-              fontSizeDef = window.getComputedStyle(document.getElementsByTagName('html')[0],null).getPropertyValue("font-size") || '16px';
+                // get the font-size from html tag
+                fontSizeDef = window.getComputedStyle(document.getElementsByTagName('html')[0],null).getPropertyValue("font-size") || '16px';
+                // get the font-size from the body
+                fontSizeBody = window.getComputedStyle(document.getElementsByTagName('body')[0],null).getPropertyValue("font-size") || '16px';
             }
             
             if (accessibilitytoolbar.userPref.get("a11yBigger") !== "keepit") {
