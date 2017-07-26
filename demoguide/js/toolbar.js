75e3423960a1c8f454f921d09a175c29d29316ab
Laussel LOïc <loic.laussel@orange.com>
2017-07-26 14:39:42 +0200

    update demo guide

--- demoguide/js/toolbar.js
+++ demoguide/js/toolbar.js
@@ -1,8 +1,8 @@
 /* orange-confort-plus - version 4.1.0 - 26-07-2017
 enhance user experience on websites
  Copyright (C) 2014 - 2017 Orange */
-var hebergementDomaine = 'https://HEBERGEMENTDOMAIN';
-var hebergementFullPath = hebergementDomaine + 'YOURPATHTOSOURCEFILES';
+var hebergementDomaine = 'http://confort-plus.orange.com';
+var hebergementFullPath = hebergementDomaine + '/demoguide/';
 // Source: app/conf/hebergement.js
 /*
 
