6cae082af8fd82f20f6740ad43ab843028b658a7
Gaël Poupard <g.poupard@groupeonepoint.com>
2023-01-25 17:31:05 +0100

    chore(MV3): almost done for Chromium-based browsers

--- dist/WebExtension/js/main.js
+++ dist/WebExtension/js/main.js
@@ -1,65 +1,64 @@
 chrome.runtime.onInstalled.addListener(() => {
-	const tabsList = {};
 	// Set CDU disable by default
-	if (chrome.storage.local.get('isCduEnabled') === null) {
-		chrome.storage.local.set('isCduEnabled', false);
-	}
-	const value = (chrome.storage.local.get('isCduEnabled') === 'true');
-	updateButtonIcon(value);
+	chrome.storage.local.get('isCduEnabled').then(result => {
+		if (result.isCduEnabled === undefined) {
+			chrome.storage.local.set({'isCduEnabled': false});
+		}
+		updateButtonIcon(result.isCduEnabled);
+	})
 	// init blacklist websites
-	if (chrome.storage.local.get('blacklist') === null) {
-		chrome.storage.local.set('blacklist', []);
-	}
+	chrome.storage.local.get('blacklist').then(result => {
+		if (result.blacklist === undefined) {
+			chrome.storage.local.set({'blacklist': []});
+		}
+	})
 });
 
-function startCDU(tab) {
-	if (localStorage.getItem('isCduEnabled') == "true") {
-		chrome.tabs.sendMessage(tab.id, {message: "orangeconfort+doyouexist"}, function (response) {
-			if (response) {
-				chrome.tabs.sendMessage(tab.id, {message: 'orangeconfort+loadcdu'});
-			}
-		});
-	}
-}
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	switch (request.message) {
-		case 'orangeconfort+userprefget' :
-			if (chrome.storage.local.get('UCI41') === null) {
-				// Default cookie value first bit set toolbar enable, second one set default lang, third one set to no profile
-				chrome.storage.local.set('UCI41', '1|0|0');
-			}
-			var index = chrome.storage.local.get('blacklist').indexOf(request.value);
-			var flag = 0;
-			if (index > -1) {
-				flag = 1;
-			}
-			chrome.tabs.sendMessage(sender.tab.id, {
-				message: 'orangeconfort+userprefgetresponse',
-				value: flag + "|" + chrome.storage.local.get('UCI41')
-			});
-			break;
-		case 'orangeconfort+userprefsave' :
-			chrome.storage.local.set('UCI41', request.value);
-			break;
-		case 'orangeconfort+blacklistsave' :
-			var blacklist = chrome.storage.local.get('blacklist');
-			var index = blacklist.indexOf(request.value);
-			if (index > -1) {
-				chrome.storage.local.set('blacklist', blacklist.splice(index, 1));
-			} else {
-				chrome.storage.local.set('blacklist', blacklist.push(value));
-			}
-			break;
-		case 'orangeconfort+getIsCduEnabled' :
-			sendResponse({value: chrome.storage.local.get('isCduEnabled')});
-			break;
-	}
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	chrome.storage.local.get('blacklist').then(result => {
+		const blacklist = result.blacklist;
+		const index = (blacklist !== undefined) ? blacklist.indexOf(request.value) : -1;
+		switch (request.message) {
+			case 'orangeconfort+userprefget':
+				chrome.storage.local.get('UCI41').then(result => {
+					if (result.UCI41 === undefined) {
+						// Default cookie value first bit set toolbar enable, second one set default lang, third one set to no profile
+						chrome.storage.local.set({'UCI41': '1|0|0'});
+					}
+					let flag = 0;
+					if (index > -1) {
+						flag = 1;
+					}
+					chrome.tabs.sendMessage(sender.tab.id, {
+						message: 'orangeconfort+userprefgetresponse',
+						value: `${flag}|${result.UCI41}`
+					});
+				})
+				break;
+			case 'orangeconfort+userprefsave':
+				chrome.storage.local.set({'UCI41': request.value});
+				break;
+			case 'orangeconfort+blacklistsave':
+				if (index > -1) {
+					chrome.storage.local.set({'blacklist': blacklist.splice(index, 1)});
+				} else {
+					chrome.storage.local.set({'blacklist': blacklist.push(value)});
+				}
+				break;
+			case 'orangeconfort+getIsCduEnabled':
+				chrome.storage.local.get('isCduEnabled').then(result => {
+					sendResponse({value: result.isCduEnabled});
+				})
+				break;
+			default:
+				break;
+		}
+	});
 });
 
 // Update CDU button icon
 function updateButtonIcon(isEnabled) {
-	if (isEnabled === true) {
+	if (isEnabled) {
 		chrome.action.setIcon({
 			path: {
 				"16": "../img/icon-16.png",
@@ -87,27 +86,32 @@ function updateButtonIcon(isEnabled) {
 }
 
 // CDU button click event
-chrome.action.onClicked.addListener(function (tab) {
-	var value = (chrome.storage.local.get('isCduEnabled') == 'true') ? false : true;
-	chrome.storage.local.set('isCduEnabled', value);
-	updateButtonIcon(value);
-	if (chrome.storage.local.get('isCduEnabled') == 'true') {
-		chrome.tabs.query({
-				"status": "complete"
-			},
-			function (tabs) {
-				for (var i = 0; i < tabs.length; i++) {
-					startCDU(tabs[i]);
+chrome.action.onClicked.addListener(tab => {
+	chrome.storage.local.get('isCduEnabled').then(result => {
+		const currentState = result.isCduEnabled;
+		chrome.storage.local.set({'isCduEnabled': !currentState});
+		updateButtonIcon(!currentState);
+		if (!currentState) {
+			chrome.tabs.query({"status": "complete"}).then(tabs => {
+				for (let i = 0; i < tabs.length; i++) {
+					const tab = tabs[i];
+					chrome.storage.local.get('isCduEnabled').then(result => {
+						if (result.isCduEnabled) {
+							chrome.tabs.sendMessage(tab.id, {message: "orangeconfort+doyouexist"}).then(response => {
+								if (response) {
+									chrome.tabs.sendMessage(tab.id, {message: 'orangeconfort+loadcdu'});
+								}
+							});
+						}
+					});
 				}
 			});
-	} else {
-		chrome.tabs.query({
-				"status": "complete"
-			},
-			function (tabs) {
+		} else {
+			chrome.tabs.query({"status": "complete"}).then(tabs => {
 				for (var i = 0; i < tabs.length; i++) {
 					chrome.tabs.sendMessage(tabs[i].id, {message: 'orangeconfort+closecdu'});
 				}
 			});
-	}
+		}
+	});
 });
--- src/WebExtension/js/hebergement.js
+++ src/WebExtension/js/hebergement.js
@@ -1,12 +1,12 @@
-var hebergementFullPath = '';
-var imagesPath = {
+let hebergementFullPath = '';
+let imagesPath = {
 	arrows: chrome.runtime.getURL('img/arrows_60.png')
 };
-var helpPath = {
+let helpPath = {
 	FR: chrome.runtime.getURL('help/help_fr.html'),
 	EN: chrome.runtime.getURL('help/help_en.html'),
 	ES: chrome.runtime.getURL('help/help_es.html'),
 	PL: chrome.runtime.getURL('help/help_pl.html')
 };
-var uci_classic_toolbar_css = chrome.runtime.getURL('css/classic-toolbar.css');
-var onOffEnabled = false;
+let uci_classic_toolbar_css = chrome.runtime.getURL('css/classic-toolbar.css');
+let onOffEnabled = false;
