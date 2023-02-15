82a7380d2932087ac34a3c1f2bd80faa30650062
Gaël Poupard <g.poupard@groupeonepoint.com>
2023-02-15 15:28:33 +0100

    chore(ESM): drop Node 12 support for build scripts

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,21 @@
-const pkg = require('./package.json');
+import * as pkg from './package.json' assert { type: "json" };
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+import banner from 'postcss-banner'
 const date = new Date();
 
-module.exports = ctx => ({
-	map: ctx.options.map,
-	plugins: {
-		'autoprefixer': {},
-		'cssnano': {},
-		'postcss-banner': {
-			banner: `${pkg.name} — version ${pkg.version} — ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}
-${pkg.description}
-© 2014 — ${date.getFullYear()} ${pkg.author}`,
-			important: true
-		}
+export default function (ctx) {
+	return {
+		map: ctx.options.map,
+		plugins: [
+			autoprefixer(),
+			cssnano(),
+			banner({
+				banner: `${pkg.name} — version ${pkg.version} — ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}
+	${pkg.description}
+	© 2014 — ${date.getFullYear()} ${pkg.author}`,
+				important: true
+			})
+		]
 	}
-})
+}
