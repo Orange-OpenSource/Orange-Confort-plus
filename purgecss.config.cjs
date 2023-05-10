9dc679e3f9ba393a4655c2fcefc5e8b243f5ee21
Gaël Poupard <g.poupard@groupeonepoint.com>
2023-05-10 17:33:26 +0200

    chore(TAC): drop TAC and GA entirely

---
+++ purgecss.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+	content: ['docs/*.html docs/help/*.html'],
+	css: ['docs/css/boosted.min.css'],
+	output: ' docs/css/boosted.min.css',
+	safelist: ['btn', 'btn-success', 'btn-danger', 'btn-close', 'btn-inverse', 'd-none', 'd-flex', 'form-check', 'form-switch', 'form-check-input', 'form-check-label', 'visually-hidden', 'ms-0', 'ms-2', 'my-2', 'my-lg-0', 'mx-sm-2', 'ms-lg-2', 'ms-lg-auto', 'my-2', 'my-lg-0', 'align-content-end', 'position-relative'],
+	variables: true,
+	fontFace: true,
+	keyframes: true
+}
