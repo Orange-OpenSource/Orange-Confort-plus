c4c5d9b65b6c1c00059cdeafec2a446b681b94e0
Gaël Poupard <g.poupard@groupeonepoint.com>
2023-10-04 15:57:22 +0200

    feat(font-family): get and apply font-faces dynamically

--- dist/serveur/js/toolbar.js
+++ dist/serveur/js/toolbar.js
@@ -439,38 +439,67 @@ tmplFontFamily.innerHTML = `
         margin-bottom: 1rem;
     }
 </style>
+<!-- @todo Loop through predefined values -->
 <button id="normal-font">Normal</button>
 <button id="arial-font">Arial</button>
 <button id="open-font-font">Open Sans</button>
 <button id="accessible-dfa-font">Accessible-DFA</button>
 <button id="open-dyslexic-font">Open Dyslexic</button>
+<button id="luciole-font">Luciole</button>
 `;
 class FontFamilyComponent extends HTMLElement {
+    // @todo Loop through predefined values
     normalBtn = null;
     arialBtn = null;
     openSansBtn = null;
     accessibleDFABtn = null;
     openDyslexicBtn = null;
+    lucioleBtn = null;
     constructor() {
         super();
         this.appendChild(tmplFontFamily.content.cloneNode(true));
+        // @ts-ignore
+        const path = this.getRootNode().host.dataset?.path;
         let head = document.head || document.getElementsByTagName('head')[0];
         let styles = document.createElement('style');
         head.appendChild(styles);
-        styles.innerHTML = '@font-face{font-family:"Accessible-DFA";src:url("./assets/fonts/AccessibleDfA-Regular.woff2");font-display:swap}@font-face{font-family:"Open-Dyslexic";src:url("./assets/fonts/OpenDyslexic-Regular.woff2");font-display:swap}@font-face{font-family:"Open-Sans";src:url("./assets/fonts/OpenSans-Regular.woff2");font-display:swap}';
+        // @todo Loop through predefined values
+        // @todo Try to handle font-size-adjust to reduce CLS?
+        styles.innerHTML = `
+			@font-face { font-family:"Accessible-DFA"; src: url("${path}fonts/accessibleDFA/AccessibleDfA-Regular.woff2"); font-style: normal; font-weight: 400; font-display:swap; }
+			@font-face { font-family:"Accessible-DFA"; src: url("${path}fonts/accessibleDFA/AccessibleDfA-Italic.woff2"); font-style: italic; font-weight: 400; font-display:swap; }
+			@font-face { font-family:"Accessible-DFA"; src: url("${path}fonts/accessibleDFA/AccessibleDfA-Bold.woff2"); font-style: normal; font-weight: 700; font-display:swap; }
+
+			@font-face { font-family:"Open-Dyslexic"; src: url("${path}fonts/open-dyslexic/OpenDyslexic-Regular.woff2"); font-style: normal; font-weight: 400; font-display:swap; }
+			@font-face { font-family:"Open-Dyslexic"; src: url("${path}fonts/open-dyslexic/OpenDyslexic-Italic.woff2"); font-style: italic; font-weight: 400; font-display:swap; }
+			@font-face { font-family:"Open-Dyslexic"; src: url("${path}fonts/open-dyslexic/OpenDyslexic-Bold.woff2"); font-style: normal; font-weight: 700; font-display:swap; }
+			@font-face { font-family:"Open-Dyslexic"; src: url("${path}fonts/open-dyslexic/OpenDyslexic-Bold-Italic.woff2"); font-style: italic; font-weight: 700; font-display:swap; }
+
+			@font-face { font-family:"Luciole"; src: url("${path}fonts/luciole/Luciole-Regular.woff2"); font-style: normal; font-weight: 400; font-display:swap; }
+			@font-face { font-family:"Luciole"; src: url("${path}fonts/luciole/Luciole-Regular-Italic.woff2"); font-style: italic; font-weight: 400; font-display:swap; }
+			@font-face { font-family:"Luciole"; src: url("${path}fonts/luciole/Luciole-Bold.woff2"); font-style: normal; font-weight: 700; font-display:swap; }
+			@font-face { font-family:"Luciole"; src: url("${path}fonts/luciole/Luciole-Bold-Italic.woff2"); font-style: italic; font-weight: 700; font-display:swap; }
+
+			@font-face { font-family:"Open-Sans"; src: url("${path}fonts/open-sans/OpenSans-Regular.woff2"); font-style: normal; font-weight: 400; font-display:swap; }
+			@font-face { font-family:"Open-Sans"; src: url("${path}fonts/open-sans/OpenSans-Italic.woff2"); font-style: italic; font-weight: 400; font-display:swap; }
+			@font-face { font-family:"Open-Sans"; src: url("${path}fonts/open-sans/OpenSans-Bold.woff2"); font-style: normal; font-weight: 700; font-display:swap; }
+			@font-face { font-family:"Open-Sans"; src: url("${path}fonts/open-sans/OpenSans-BoldItalic.woff2"); font-style: italic; font-weight: 700; font-display:swap; }`;
+        // @todo Loop through predefined values, could be radio buttons
         this.normalBtn = this.querySelector('#normal-font');
         this.arialBtn = this.querySelector('#arial-font');
         this.openSansBtn = this.querySelector('#open-font-font');
         this.accessibleDFABtn = this.querySelector('#accessible-dfa-font');
         this.openDyslexicBtn = this.querySelector('#open-dyslexic-font');
+        this.lucioleBtn = this.querySelector('#luciole-font');
     }
     connectedCallback() {
         const bodyElt = document.getElementsByTagName('body')[0];
+        // @todo Loop through predefined values, could be radio buttons
         this.normalBtn?.addEventListener('click', () => {
             bodyElt.style.fontFamily = ``;
         });
         this.arialBtn?.addEventListener('click', () => {
-            bodyElt.style.fontFamily = `arial`;
+            bodyElt.style.fontFamily = `"Liberation Sans", Arial, sans-serif`;
         });
         this.openSansBtn?.addEventListener('click', () => {
             bodyElt.style.fontFamily = `Open-Sans`;
@@ -481,8 +510,12 @@ class FontFamilyComponent extends HTMLElement {
         this.openDyslexicBtn?.addEventListener('click', () => {
             bodyElt.style.fontFamily = `Open-Dyslexic`;
         });
+        this.lucioleBtn?.addEventListener('click', () => {
+            bodyElt.style.fontFamily = `Luciole`;
+        });
     }
     disconnectedCallback() {
+        // @todo Loop through predefined values, could be radio buttons
         this.normalBtn?.removeEventListener('click', () => {
         });
         this.arialBtn?.removeEventListener('click', () => {
@@ -492,6 +525,8 @@ class FontFamilyComponent extends HTMLElement {
         this.accessibleDFABtn?.removeEventListener('click', () => {
         });
         this.openDyslexicBtn?.removeEventListener('click', () => {
+        });
+        this.lucioleBtn?.removeEventListener('click', () => {
         });
     }
 }
@@ -1011,5 +1046,6 @@ class ToolbarComponent extends HTMLElement {
 }
 customElements.define('app-toolbar', ToolbarComponent);
 const appRootElt = document.createElement('app-root');
+// @todo Allow path to be configured
 appRootElt.dataset.path = window.location.origin + '/';
 document.body.prepend(appRootElt);
