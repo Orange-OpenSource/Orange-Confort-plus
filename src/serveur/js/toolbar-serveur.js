8da253ec75c109f4954cda83d71215eb66209010
Gaël Poupard <g.poupard@groupeonepoint.com>
2023-10-04 15:57:22 +0200

    feat(font-family): get and apply font-faces dynamically

--- src/serveur/js/toolbar-serveur.js
+++ src/serveur/js/toolbar-serveur.js
@@ -1,3 +1,4 @@
 const appRootElt = document.createElement('app-root');
+// @todo Allow path to be configured
 appRootElt.dataset.path = window.location.origin + '/';
 document.body.prepend(appRootElt);
