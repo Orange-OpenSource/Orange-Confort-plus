ff0e39b767e9e5f64ea99dd45b01440ffa9415e5
Gaël Poupard <g.poupard@groupeonepoint.com>
2023-10-18 17:24:22 +0200

    feat(i18n)[WIP]: trying to mimic chrome.i18n in browser…

--- src/serveur/js/service-i18n.js
+++ src/serveur/js/service-i18n.js
@@ -1,9 +1,29 @@
 class i18nService {
-	constructor() {
-		console.log("Hello world!");
+	locale = 'en';
+	path = '';
+
+	constructor(path) {
+		this.path = path;
+
+		if(['en', 'fr'].some(language => navigator.language.startsWith(language))) {
+			this.locale = navigator.language.slice(0,2);
+		}
 	}
 
-	getMessage = () => {
-		console.log('Hello server!');
+	get locale() {
+		return this.locale;
+	}
+
+	// @fixme fetch() is shitty since it's Promise-based
+	// @todo Need to append locales as a massive json file in Server build…
+	async getMessage(message) {
+	 try {
+		 const response = await fetch(`${this.path}_locales/${this.locale}/messages.json`);
+		 const data = await response.json();
+
+		 return data[message]?.message;
+	 } catch (error) {
+		 throw new Error(`Couldn’t find ${message} in ${this.locale}`);
+	 }
 	}
 }
