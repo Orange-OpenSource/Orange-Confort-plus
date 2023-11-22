800164997b3b3d6d60d2ff35688bfe50b6bcaa9c
Thomas DELIERE <t.deliere@groupeonepoint.com>
2023-11-22 11:05:34 +0100

    Prise en compte des retours.

--- src/app/shared/select-mode.component.ts
+++ src/app/shared/select-mode.component.ts
@@ -1,56 +1,29 @@
 const selectModeLayout: HTMLTemplateElement = document.createElement('template');
 selectModeLayout.innerHTML = `
-	<style>
-		label {
-			width: 100%;
-			cursor: pointer;
-		}
-
-		input {
-			appearance: none;
-    	-webkit-appearance: none;
-    	-moz-appearance: none;
-			position: absolute;
-		}
-
-		input:checked + div {
-			box-shadow: var(--bs-focus-ring-x,0) var(--bs-focus-ring-y,0) var(--bs-focus-ring-blur,0) var(--bs-focus-ring-width) var(--bs-focus-ring-color);
-		}
-
-		input:not(:checked) + div > p {
-			display: none;
-		}
-	</style>
-
-	<label>
-		<input type="radio">
-		<div class="d-flex flex-column gap-1 p-1">
-			<div class="d-flex align-items-center gap-2">
-				<app-icon data-size="2rem"></app-icon>
-				<span class="fs-5 text"></span>
-			</div>
-			<p class="fs-6 fw-normal m-0"></p>
+	<input type="radio" class="sc-select-mode__input">
+	<label class="d-flex flex-column gap-1 p-1 sc-select-mode__label">
+		<div class="d-flex align-items-center gap-2">
+			<app-icon data-size="2rem"></app-icon>
+			<span class="fs-5 text"></span>
 		</div>
+		<p class="fs-6 fw-normal m-0"></p>
 	</label>
 `;
 
 class SelectModeComponent extends HTMLElement {
-	inputElement: HTMLElement | null = null;
+	inputElement: HTMLInputElement | null = null;
 	iconElement: HTMLElement | null = null;
-	labelElement: HTMLElement | null = null;
-	descriptionElement: HTMLElement | null = null;
-	id = '';
+	labelElement: HTMLLabelElement | null = null;
+	textElement: HTMLElement | null = null;
+	descriptionElement: HTMLParagraphElement | null = null;
 	icon = '';
-	name = '';
 	label = '';
 	description = '';
 
 	constructor() {
 		super();
 
-		this.id = this.dataset?.id || this.id;
 		this.icon = this.dataset?.icon || this.icon;
-		this.name = this.dataset?.name || this.name;
 		this.label = this.dataset?.label || this.label;
 		this.description = this.dataset?.description || this.description;
 
@@ -59,15 +32,17 @@ class SelectModeComponent extends HTMLElement {
 
 	connectedCallback(): void {
 		this.inputElement = this.querySelector('input');
+		this.labelElement = this.querySelector('label');
 		this.iconElement = this.querySelector('app-icon');
-		this.labelElement = this.querySelector('span');
+		this.textElement = this.querySelector('span');
 		this.descriptionElement = this.querySelector('p');
 
-		this.inputElement?.setAttribute('id', this.id);
-		this.inputElement?.setAttribute('name', this.name);
+		this.inputElement!.id = this.dataset?.id || '';
+		this.inputElement!.name = this.dataset?.name || '';
+		this.labelElement!.setAttribute('for', this.dataset?.id || '');
 		this.iconElement!.dataset.name = this.icon;
-		this.labelElement!.innerHTML = this.label;
-		this.descriptionElement!.innerHTML = this.description;
+		this.textElement!.innerText = this.label;
+		this.descriptionElement!.innerText = this.description;
 	}
 }
 
