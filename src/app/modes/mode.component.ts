c8568bba92d412c64593c06ad14d9eec1c465c24
Thomas DELIERE <t.deliere@groupeonepoint.com>
2023-12-06 16:50:30 +0100

    Test de récupération d'un JSON dans app-mode pour afficher les réglages.

--- src/app/modes/mode.component.ts
+++ src/app/modes/mode.component.ts
@@ -12,38 +12,41 @@ class ModeComponent extends HTMLElement {
 	static observedAttributes = ['data-parameters'];
 	modeContent: HTMLElement | null = null;
 
-	parameters = [
-		{ name: 'Taille de texte', values: '16, 18, 20, 22, 24' },
-		{ name: 'Graisse', values: 'light, regular, bold' },
-		{ name: 'Font', values: 'Nom font 1, Nom font 2, Nom font 3' },
-	];
-
 	constructor() {
 		super();
 
 		this.appendChild(tmplMode.content.cloneNode(true));
 	}
 
-	connectedCallback(): void {
+	setParameters(mode: JSON): void {
 		this.modeContent = this.querySelector('#mode-content');
-		this.setParameters();
-	}
-
-	setParameters(): void {
 		let btnSettingList = '';
-		this.parameters?.forEach((parameter) => {
-			const label = parameter?.name;
-			const settingsList = parameter?.values;
-			let btnSetting = `<app-btn-setting data-label="${label}" data-settings-list="${settingsList}"></app-btn-setting>`;
-			btnSettingList = btnSettingList + btnSetting;
-		});
+		let label = '';
 
-		this.modeContent!.innerHTML = btnSettingList;
+		if (this.modeContent) {
+			// @ts-ignore
+			Object.entries(mode.parameters).forEach(([key, value]) => {
+				let listValue = '';
+				// @ts-ignore
+				label = value['name'];
+				// @ts-ignore
+				Object.entries(value).forEach(([key, value]) => {
+					// @ts-ignore
+					if (key.indexOf('value') > -1) {
+						listValue ? listValue = `${listValue},${value}` : listValue = `${value}`;
+					}
+				});
+				let btnSetting = `<app-btn-setting data-label="${label}" data-settings-list="${listValue}"></app-btn-setting>`;
+				btnSettingList = btnSettingList + btnSetting;
+			});
+
+			this.modeContent.innerHTML = btnSettingList;
+		}
 	}
 
 	attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
 		if ('data-parameters' === name) {
-			this.setParameters();
+			this.setParameters(JSON.parse(newValue));
 		}
 	}
 }
