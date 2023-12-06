f74faecc8caa37436d90b0fb0d086a8530aef2af
Thomas DELIERE <t.deliere@groupeonepoint.com>
2023-12-06 14:39:12 +0100

    Correction suite retour Gaël

---
+++ src/extension/services/i18n.service.js
@@ -0,0 +1,23 @@
+class i18nService {
+	constructor() {}
+
+	get locale() {
+		return chrome.i18n.getUILanguage();
+	}
+
+	getMessage = (message) => {
+		return chrome.i18n.getMessage(message);
+	}
+
+	translate(root) {
+		const elements = root.querySelectorAll('[data-i18n]');
+		for (const element of elements) {
+			element.innerHTML = this.getMessage(element.dataset?.i18n);
+		}
+
+		const elementsTitle = root.querySelectorAll('[data-i18n-title]');
+		for (const element of elementsTitle) {
+			element.title = this.getMessage(element.dataset?.i18nTitle);
+		}
+	}
+}
