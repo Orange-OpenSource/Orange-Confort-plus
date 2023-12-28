0ffff950b375a025ee4fccaaf49ee8dc1326bdab
Thomas DELIERE <t.deliere@groupeonepoint.com>
2023-12-28 14:11:32 +0100

    Revu de la fonctionnalité de modification de police de texte.

--- dist/serveur/js/toolbar.js
+++ dist/serveur/js/toolbar.js
@@ -216,62 +216,235 @@ class AbstractSetting extends HTMLElement {
 
 const tmplFontFamily = document.createElement("template");
 
-tmplFontFamily.innerHTML = `\n<style>\n\tapp-font-family {\n\t\t\tmargin-bottom: 1rem;\n\t}\n</style>\n\x3c!-- @todo Loop through predefined values --\x3e\n\x3c!-- @note To translate, or not? --\x3e\n<button id="normal-font" data-i18n="default"></button>\n<button id="arial-font">Arial</button>\n<button id="open-font-font">Open Sans</button>\n<button id="accessible-dfa-font">Accessible-DFA</button>\n<button id="open-dyslexic-font">Open Dyslexic</button>\n<button id="luciole-font">Luciole</button>\n`;
+tmplFontFamily.innerHTML = `\n<div class="d-flex">\n\t<app-btn-setting data-label="textFont" data-icon="Police"></app-btn-setting>\n\t<app-btn-modal class="d-none"></app-btn-modal>\n</div>\n`;
 
-class FontFamilyComponent extends HTMLElement {
-    normalBtn=null;
-    arialBtn=null;
-    openSansBtn=null;
-    accessibleDFABtn=null;
-    openDyslexicBtn=null;
-    lucioleBtn=null;
+class FontFamilyComponent extends AbstractSetting {
     pathService;
     path;
+    fontDictionnary=[ {
+        name: "Accessible-DFA",
+        folder: "accessibleDFA",
+        files: [ {
+            name: "AccessibleDFA-Bold.woff2",
+            style: "normal",
+            weight: "700"
+        }, {
+            name: "AccessibleDFA-Italic.woff2",
+            style: "italic",
+            weight: "400"
+        }, {
+            name: "AccessibleDFA-Regular.woff2",
+            style: "normal",
+            weight: "400"
+        } ]
+    }, {
+        name: "B612 Mono",
+        folder: "B612",
+        files: [ {
+            name: "B612Mono-Bold.ttf",
+            style: "normal",
+            weight: "700"
+        }, {
+            name: "B612Mono-BoldItalic.ttf",
+            style: "italic",
+            weight: "700"
+        }, {
+            name: "B612Mono-Italic.ttf",
+            style: "italic",
+            weight: "400"
+        }, {
+            name: "B612Mono-Regular.ttf",
+            style: "normal",
+            weight: "400"
+        } ]
+    }, {
+        name: "Comic Sans",
+        folder: "comic",
+        files: [ {
+            name: "comic-sans.woff",
+            style: "normal",
+            weight: "400"
+        } ]
+    }, {
+        name: "Lexand Deca",
+        folder: "lexendDeca",
+        files: [ {
+            name: "LexendDeca-Black.ttf",
+            style: "normal",
+            weight: "900"
+        }, {
+            name: "LexendDeca-Bold.ttf",
+            style: "normal",
+            weight: "700"
+        }, {
+            name: "LexendDeca-ExtraBold.ttf",
+            style: "normal",
+            weight: "800"
+        }, {
+            name: "LexendDeca-ExtraLight.ttf",
+            style: "normal",
+            weight: "200"
+        }, {
+            name: "LexendDeca-Light.ttf",
+            style: "normal",
+            weight: "300"
+        }, {
+            name: "LexendDeca-Medium.ttf",
+            style: "normal",
+            weight: "500"
+        }, {
+            name: "LexendDeca-Regular.ttf",
+            style: "normal",
+            weight: "400"
+        }, {
+            name: "LexendDeca-SemiBold.ttf",
+            style: "normal",
+            weight: "600"
+        }, {
+            name: "LexendDeca-Thin.ttf",
+            style: "normal",
+            weight: "100"
+        } ]
+    }, {
+        name: "Luciole",
+        folder: "luciole",
+        files: [ {
+            name: "Luciole-Bold-Italic.woff2",
+            style: "italic",
+            weight: "700"
+        }, {
+            name: "Luciole-Bold.woff2",
+            style: "normal",
+            weight: "700"
+        }, {
+            name: "Luciole-Regular-Italic.woff2",
+            style: "italic",
+            weight: "400"
+        }, {
+            name: "Luciole-Regular.woff2",
+            style: "normal",
+            weight: "400"
+        } ]
+    }, {
+        name: "Sylexiad Sans",
+        folder: "sylexiadSans",
+        files: [ {
+            name: "SylexiadSansMedium-BoldItalic.woff",
+            style: "italic",
+            weight: "700"
+        }, {
+            name: "SylexiadSansMedium-Bold.woff",
+            style: "normal",
+            weight: "700"
+        }, {
+            name: "SylexiadSansMedium-Italic.woff",
+            style: "italic",
+            weight: "400"
+        }, {
+            name: "SylexiadSansMedium.woff",
+            style: "normal",
+            weight: "400"
+        }, {
+            name: "SylexiadSansSpacedMed-BoldItalic.woff",
+            style: "italic",
+            weight: "700"
+        }, {
+            name: "SylexiadSansSpacedMed-Bold.woff",
+            style: "normal",
+            weight: "700"
+        }, {
+            name: "SylexiadSansSpacedMed-Italic.woff",
+            style: "italic",
+            weight: "400"
+        }, {
+            name: "SylexiadSansSpacedMed.woff",
+            style: "normal",
+            weight: "400"
+        }, {
+            name: "SylexiadSansSpacedThin-BoldItalic.woff",
+            style: "italic",
+            weight: "700"
+        }, {
+            name: "SylexiadSansSpacedThin-Bold.woff",
+            style: "normal",
+            weight: "700"
+        }, {
+            name: "SylexiadSansSpacedThin-Italic.woff",
+            style: "italic",
+            weight: "400"
+        }, {
+            name: "SylexiadSansSpacedThin.woff",
+            style: "normal",
+            weight: "400"
+        }, {
+            name: "SylexiadSansThin-BoldItalic.woff",
+            style: "italic",
+            weight: "700"
+        }, {
+            name: "SylexiadSansThin-Bold.woff",
+            style: "normal",
+            weight: "700"
+        }, {
+            name: "SylexiadSansThin-Italic.woff",
+            style: "italic",
+            weight: "400"
+        }, {
+            name: "SylexiadSansThin.woff.woffff2",
+            style: "normal",
+            weight: "400"
+        } ]
+    }, {
+        name: "Verdana",
+        folder: "verdana",
+        files: [ {
+            name: "Verdana-Bold-Italic.woff2",
+            style: "italic",
+            weight: "700"
+        }, {
+            name: "Verdana-Bold.woff2",
+            style: "normal",
+            weight: "700"
+        }, {
+            name: "Verdana-Italic.woff2",
+            style: "italic",
+            weight: "400"
+        }, {
+            name: "Verdana.woff2",
+            style: "normal",
+            weight: "400"
+        } ]
+    } ];
     constructor() {
         super();
-        this.appendChild(tmplFontFamily.content.cloneNode(true));
         this.pathService = new PathService;
         this.path = this.pathService.path;
+        this.appendChild(tmplFontFamily.content.cloneNode(true));
         let head = document.head || document.getElementsByTagName("head")[0];
         let styles = document.createElement("style");
         head.appendChild(styles);
-        styles.innerHTML = `\n\t\t\t@font-face { font-family:"Accessible-DFA"; src: url("${this.path}assets/fonts/accessibleDFA/AccessibleDfA-Regular.woff2"); font-style: normal; font-weight: 400; font-display:swap; }\n\t\t\t@font-face { font-family:"Accessible-DFA"; src: url("${this.path}assets/fonts/accessibleDFA/AccessibleDfA-Italic.woff2"); font-style: italic; font-weight: 400; font-display:swap; }\n\t\t\t@font-face { font-family:"Accessible-DFA"; src: url("${this.path}assets/fonts/accessibleDFA/AccessibleDfA-Bold.woff2"); font-style: normal; font-weight: 700; font-display:swap; }\n\n\t\t\t@font-face { font-family:"Open-Dyslexic"; src: url("${this.path}assets/fonts/open-dyslexic/OpenDyslexic-Regular.woff2"); font-style: normal; font-weight: 400; font-display:swap; }\n\t\t\t@font-face { font-family:"Open-Dyslexic"; src: url("${this.path}assets/fonts/open-dyslexic/OpenDyslexic-Italic.woff2"); font-style: italic; font-weight: 400; font-display:swap; }\n\t\t\t@font-face { font-family:"Open-Dyslexic"; src: url("${this.path}assets/fonts/open-dyslexic/OpenDyslexic-Bold.woff2"); font-style: normal; font-weight: 700; font-display:swap; }\n\t\t\t@font-face { font-family:"Open-Dyslexic"; src: url("${this.path}assets/fonts/open-dyslexic/OpenDyslexic-Bold-Italic.woff2"); font-style: italic; font-weight: 700; font-display:swap; }\n\n\t\t\t@font-face { font-family:"Luciole"; src: url("${this.path}assets/fonts/luciole/Luciole-Regular.woff2"); font-style: normal; font-weight: 400; font-display:swap; }\n\t\t\t@font-face { font-family:"Luciole"; src: url("${this.path}assets/fonts/luciole/Luciole-Regular-Italic.woff2"); font-style: italic; font-weight: 400; font-display:swap; }\n\t\t\t@font-face { font-family:"Luciole"; src: url("${this.path}assets/fonts/luciole/Luciole-Bold.woff2"); font-style: normal; font-weight: 700; font-display:swap; }\n\t\t\t@font-face { font-family:"Luciole"; src: url("${this.path}assets/fonts/luciole/Luciole-Bold-Italic.woff2"); font-style: italic; font-weight: 700; font-display:swap; }\n\n\t\t\t@font-face { font-family:"Open-Sans"; src: url("${this.path}assets/fonts/open-sans/OpenSans-Regular.woff2"); font-style: normal; font-weight: 400; font-display:swap; }\n\t\t\t@font-face { font-family:"Open-Sans"; src: url("${this.path}assets/fonts/open-sans/OpenSans-Italic.woff2"); font-style: italic; font-weight: 400; font-display:swap; }\n\t\t\t@font-face { font-family:"Open-Sans"; src: url("${this.path}assets/fonts/open-sans/OpenSans-Bold.woff2"); font-style: normal; font-weight: 700; font-display:swap; }\n\t\t\t@font-face { font-family:"Open-Sans"; src: url("${this.path}assets/fonts/open-sans/OpenSans-BoldItalic.woff2"); font-style: italic; font-weight: 700; font-display:swap; }`;
-        this.normalBtn = this.querySelector("#normal-font");
-        this.arialBtn = this.querySelector("#arial-font");
-        this.openSansBtn = this.querySelector("#open-font-font");
-        this.accessibleDFABtn = this.querySelector("#accessible-dfa-font");
-        this.openDyslexicBtn = this.querySelector("#open-dyslexic-font");
-        this.lucioleBtn = this.querySelector("#luciole-font");
+        const fontFaceList = [];
+        this.fontDictionnary.forEach((font => {
+            for (const file of font.files) {
+                fontFaceList.push(`@font-face { font-family:"${font.name}"; src: url("${this.path}assets/fonts/${font.folder}/${file.name}"); font-style: ${file.style}; font-weight: ${file.weight}; font-display: swap; }`);
+            }
+        }));
+        styles.innerHTML = fontFaceList.join("");
     }
     connectedCallback() {
-        const bodyElt = document.getElementsByTagName("body")[0];
-        this.normalBtn?.addEventListener("click", (() => {
-            bodyElt.style.fontFamily = ``;
-        }));
-        this.arialBtn?.addEventListener("click", (() => {
-            bodyElt.style.fontFamily = `"Liberation Sans", Arial, sans-serif`;
-        }));
-        this.openSansBtn?.addEventListener("click", (() => {
-            bodyElt.style.fontFamily = `Open-Sans`;
-        }));
-        this.accessibleDFABtn?.addEventListener("click", (() => {
-            bodyElt.style.fontFamily = `Accessible-DFA`;
-        }));
-        this.openDyslexicBtn?.addEventListener("click", (() => {
-            bodyElt.style.fontFamily = `Open-Dyslexic`;
-        }));
-        this.lucioleBtn?.addEventListener("click", (() => {
-            bodyElt.style.fontFamily = `Luciole`;
+        super.connectedCallback();
+        this.settingBtn.addEventListener("changeSettingEvent", (event => {
+            this.setFontFamily(event.detail.value);
         }));
     }
     disconnectedCallback() {
-        this.normalBtn?.removeEventListener("click", (() => {}));
-        this.arialBtn?.removeEventListener("click", (() => {}));
-        this.openSansBtn?.removeEventListener("click", (() => {}));
-        this.accessibleDFABtn?.removeEventListener("click", (() => {}));
-        this.openDyslexicBtn?.removeEventListener("click", (() => {}));
-        this.lucioleBtn?.removeEventListener("click", (() => {}));
+        super.disconnectedCallback();
+        this.settingBtn.removeEventListener("changeSettingEvent", (() => {}));
     }
+    setFontFamily=value => {
+        const bodyElt = document.getElementsByTagName("body")[0];
+        bodyElt.style.fontFamily = value;
+        this.modalBtn.setAttribute("data-value", value);
+    };
 }
 
 customElements.define("app-font-family", FontFamilyComponent);
@@ -483,7 +656,7 @@ customElements.define("app-btn-modal", BtnModalComponent);
 
 const btnSettingLayout = document.createElement("template");
 
-btnSettingLayout.innerHTML = `\n\t<button class="sc-btn-setting btn btn-primary flex-column w-100">\n\t\t<span></span>\n\t\t<app-icon data-name="Text_Size"></app-icon>\n\t\t<ul class="d-flex gap-1 align-items-center mt-2 mb-0 list-unstyled"></ul>\n\t</button>\n`;
+btnSettingLayout.innerHTML = `\n\t<button class="sc-btn-setting btn btn-primary flex-column w-100">\n\t\t<span></span>\n\t\t<app-icon></app-icon>\n\t\t<ul class="d-flex gap-1 align-items-center mt-2 mb-0 list-unstyled"></ul>\n\t</button>\n`;
 
 class BtnSettingComponent extends HTMLElement {
     static observedAttributes=[ "data-values", "data-active-value", "data-label", "data-icon" ];
