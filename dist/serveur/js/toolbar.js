a3848d2568219a7e87e3b9416882bb8ccae28093
Thomas DELIERE <t.deliere@groupeonepoint.com>
2024-01-03 21:40:02 +0100

    Ajout de la fonctionnalité aspect de la souris

--- dist/serveur/js/toolbar.js
+++ dist/serveur/js/toolbar.js
@@ -219,8 +219,10 @@ const tmplColorContrast = document.createElement("template");
 tmplColorContrast.innerHTML = `\n<div class="d-flex align-items-center gap-3">\n\t<app-btn-setting data-label="colorsContrasts" data-icon="Contrast"></app-btn-setting>\n\t<app-btn-modal class="d-none"></app-btn-modal>\n</div>\n`;
 
 class ColorContrastComponent extends AbstractSetting {
+    i18nService;
     constructor() {
         super();
+        this.i18nService = new I18nService;
         this.appendChild(tmplColorContrast.content.cloneNode(true));
     }
     connectedCallback() {
@@ -240,20 +242,20 @@ class ColorContrastComponent extends AbstractSetting {
         } else {
             let color = "";
             let backgroundColor = "";
-            if (value === "renforced") {
+            if (value === "reinforced") {
                 color = "#000";
                 backgroundColor = "#fff";
-                label = `Contrasts renforcés`;
+                label = this.i18nService.getMessage("reinforcedContrasts");
             } else if (value === "daltonism") {
                 color = "#000";
                 backgroundColor = "#fff";
-                label = `Mode daltonien`;
+                label = this.i18nService.getMessage("daltonism");
             } else {
                 color = value.split("+")[0];
                 backgroundColor = value.split("+")[1];
                 label = `${color} / ${backgroundColor}`;
             }
-            let classContrast = `\n\t\t\t\t\t\t\t.cplus-color-contrast *,\n\t\t\t\t\t\t\t.cplus-color-contrast li a {\n\t\t\t\t\t\t\t\tcolor: ${color} !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\t.cplus-color-contrast fieldset,\n\t\t\t\t\t\t\t.cplus-color-contrast button,\n\t\t\t\t\t\t\t.cplus-color-contrast input {\n\t\t\t\t\t\t\t\tborder-color: ${color} !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\t.cplus-color-contrast input {\n\t\t\t\t\t\t\t\tborder-width: 2px !important;\n\t\t\t\t\t\t\t\tborder-style: solid !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\t.cplus-color-contrast td, .cplus-color-contrast th {\n\t\t\t\t\t\t\t\tborder: 2px solid ${color} !important;\n\t\t\t\t\t\t\t\tpadding: .2em !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\t.cplus-color-contrast table {\n\t\t\t\t\t\t\t\tborder-collapse: collapse !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\t.cplus-color-contrast *:link,\n\t\t\t\t\t\t\t.cplus-color-contrast *:visited,\n\t\t\t\t\t\t\t.cplus-color-contrast *:hover {\n\t\t\t\t\t\t\t\tcolor: ${color} !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\t* {\n\t\t\t\t\t\t\t\tcolor: ${color} !important;\n\t\t\t\t\t\t\t\tbackground-color: ${backgroundColor} !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\tli a {\n\t\t\t\t\t\t\t\tcolor: ${color} !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\tfieldset,\n\t\t\t\t\t\t\tbutton,\n\t\t\t\t\t\t  input {\n\t\t\t\t\t\t\t\tborder-color: ${color} !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\tinput {\n\t\t\t\t\t\t\t\tborder-width: 2px !important;\n\t\t\t\t\t\t\t\tborder-style: solid !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\ttd, th {\n\t\t\t\t\t\t\t\tborder: 2px solid ${color} !important;\n\t\t\t\t\t\t\t\tpadding: .2em !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\ttable {\n\t\t\t\t\t\t\t\tborder-collapse: collapse !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\t*:link,\n\t\t\t\t\t\t\t*:visited,\n\t\t\t\t\t\t\t*:hover {\n\t\t\t\t\t\t\t\tcolor: ${color} !important;\n\t\t\t\t\t\t\t}\n\t\t\t\t\t\t`;
+            let classContrast = `\n\t\t\t\t\t\t\t* {\n\t\t\t\t\t\t\t\tcolor: ${color} !important;\n\t\t\t\t\t\t\t\tbackground-color: ${backgroundColor} !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\tli a {\n\t\t\t\t\t\t\t\tcolor: ${color} !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\tfieldset,\n\t\t\t\t\t\t\tbutton,\n\t\t\t\t\t\t  input {\n\t\t\t\t\t\t\t\tborder-color: ${color} !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\tinput {\n\t\t\t\t\t\t\t\tborder-width: 2px !important;\n\t\t\t\t\t\t\t\tborder-style: solid !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\ttd, th {\n\t\t\t\t\t\t\t\tborder: 2px solid ${color} !important;\n\t\t\t\t\t\t\t\tpadding: .2em !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\ttable {\n\t\t\t\t\t\t\t\tborder-collapse: collapse !important;\n\t\t\t\t\t\t\t}\n\n\t\t\t\t\t\t\t*:link,\n\t\t\t\t\t\t\t*:visited,\n\t\t\t\t\t\t\t*:hover {\n\t\t\t\t\t\t\t\tcolor: ${color} !important;\n\t\t\t\t\t\t\t}\n\t\t\t\t\t\t`;
             if (document.querySelectorAll("#cplus-styles-contrast").length === 0) {
                 let head = document.head || document.getElementsByTagName("head")[0];
                 let stylesContrast = document.createElement("style");
@@ -269,6 +271,51 @@ class ColorContrastComponent extends AbstractSetting {
 }
 
 customElements.define("app-color-contrast", ColorContrastComponent);
+
+"use strict";
+
+const tmplCursorAspect = document.createElement("template");
+
+tmplCursorAspect.innerHTML = `\n<div class="d-flex align-items-center gap-3">\n\t<app-btn-setting data-label="cursorAspect" data-icon="CursorSetting"></app-btn-setting>\n\t<app-btn-modal class="d-none"></app-btn-modal>\n</div>\n`;
+
+class CursorAspectComponent extends AbstractSetting {
+    constructor() {
+        super();
+        this.appendChild(tmplCursorAspect.content.cloneNode(true));
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        this.settingBtn.addEventListener("changeSettingEvent", (event => {
+            this.setCursor(event.detail.value);
+        }));
+    }
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        this.settingBtn.removeEventListener("changeSettingEvent", (() => {}));
+    }
+    setCursor=value => {
+        let label = value;
+        if (value === "default" && document.querySelector("#cplus-styles-cursor")) {
+            document.querySelector("#cplus-styles-cursor").remove();
+        } else {
+            let color = value.split("+")[1];
+            let svgFile = value.split("+")[0] === "big" ? `<svg width="56" height="56" viewbox="0 0 56 56" xmlns="http://www.w3.org/2000/svg"><path d="M2.1875 3.93386C2.1875 3.07793 3.19283 2.61758 3.84083 3.17679L35.3653 30.3817C36.039 30.9631 35.6748 32.0687 34.7875 32.1359L22.9084 33.0354C22.2046 33.0887 21.776 33.8346 22.0844 34.4695L29.2853 49.2945C29.5344 49.8073 29.3051 50.4242 28.7816 50.6498L22.336 53.4272C21.8383 53.6416 21.2604 53.4206 21.0328 52.9288L14.1035 37.9578C13.8313 37.3697 13.0802 37.1919 12.5732 37.5955L3.81035 44.572C3.15521 45.0936 2.1875 44.6271 2.1875 43.7897V3.93386Z" fill="${color}" stroke="black" stroke-width="5"/></svg>` : `<svg width="128" height="128" viewbox="0 0 128 128" xmlns="http://www.w3.org/2000/svg"><path d="M5 6.18386C5 5.32793 6.00533 4.86758 6.65333 5.42679L83.1778 71.4657C83.8515 72.0471 83.4873 73.1527 82.6 73.2199L50.4764 75.6523C49.7726 75.7056 49.344 76.4515 49.6524 77.0864L67.5257 113.883C67.7748 114.396 67.5455 115.013 67.0219 115.238L49.904 122.615C49.4063 122.829 48.8284 122.608 48.6008 122.116L31.5493 85.2757C31.2771 84.6875 30.5259 84.5097 30.0189 84.9134L6.62285 103.54C5.96772 104.062 5 103.595 5 102.758V6.18386Z" fill="${color}" stroke="black" stroke-width="10"/></svg>`;
+            let classCursor = `\n\t\t\t\t\t\t\t* {\n\t\t\t\t\t\t\t\tcursor: url('data:image/svg+xml;utf8,${svgFile}') 0 0, auto !important;\n\t\t\t\t\t\t\t}\n\t\t\t\t\t\t`;
+            if (document.querySelectorAll("#cplus-styles-cursor").length === 0) {
+                let head = document.head || document.getElementsByTagName("head")[0];
+                let stylesCursor = document.createElement("style");
+                stylesCursor.setAttribute("id", "cplus-styles-cursor");
+                stylesCursor.innerHTML = classCursor;
+                head.appendChild(stylesCursor);
+            } else {
+                document.querySelector("#cplus-styles-cursor").innerHTML = classCursor;
+            }
+        }
+        this.modalBtn.setAttribute("data-value", label);
+    };
+}
+
+customElements.define("app-cursor-aspect", CursorAspectComponent);
 
 "use strict";
 
@@ -1187,7 +1234,7 @@ customElements.define("app-home", HomeComponent);
 
 const tmplMode = document.createElement("template");
 
-tmplMode.innerHTML = `\n<div id="mode-content" class="sc-mode__setting-grid gap-2">\n\t<app-font-family class="c-mode__setting"></app-font-family>\n\t<app-increase-text-size class="c-mode__setting"></app-increase-text-size>\n\t<app-spacing-text class="c-mode__setting"></app-spacing-text>\n\t<app-reading-guide class="c-mode__setting"></app-reading-guide>\n\t<app-margin-align class="c-mode__setting"></app-margin-align>\n\t<app-focus-aspect class="c-mode__setting"></app-focus-aspect>\n\t<app-color-contrast class="c-mode__setting"></app-color-contrast>\n</div>\n`;
+tmplMode.innerHTML = `\n<div id="mode-content" class="sc-mode__setting-grid gap-2">\n\t<app-font-family class="c-mode__setting"></app-font-family>\n\t<app-increase-text-size class="c-mode__setting"></app-increase-text-size>\n\t<app-spacing-text class="c-mode__setting"></app-spacing-text>\n\t<app-reading-guide class="c-mode__setting"></app-reading-guide>\n\t<app-margin-align class="c-mode__setting"></app-margin-align>\n\t<app-focus-aspect class="c-mode__setting"></app-focus-aspect>\n\t<app-color-contrast class="c-mode__setting"></app-color-contrast>\n\t<app-cursor-aspect class="c-mode__setting"></app-cursor-aspect>\n</div>\n`;
 
 class ModeComponent extends HTMLElement {
     static observedAttributes=[ "data-settings" ];
@@ -1213,6 +1260,9 @@ class ModeComponent extends HTMLElement {
     }, {
         name: "colorContrast",
         element: "app-color-contrast"
+    }, {
+        name: "cursorAspect",
+        element: "app-cursor-aspect"
     } ];
     constructor() {
         super();
@@ -1476,9 +1526,16 @@ tmplPointer.innerHTML = `\n\t<div class="accordion-item">\n\t\t<div class="accor
 
 class PointerComponent extends AbstractCategory {
     constructor() {
-        let settingsDictionnary = [];
+        const settingsDictionnary = [ {
+            name: "cursorAspect",
+            element: "app-cursor-aspect"
+        } ];
         super(settingsDictionnary);
         this.appendChild(tmplPointer.content.cloneNode(true));
+    }
+    connectedCallback() {
+        let settingsElements = [ ...this.querySelectorAll(".c-pointer__setting") ];
+        super.connectedCallback(settingsElements);
     }
 }
 
