0dd5bfba56d6bc2e7523ef6158de70aed6f934a4
Thomas DELIERE <t.deliere@groupeonepoint.com>
2024-01-24 16:05:57 +0100

    Ajout du bouton pause.

---
+++ src/app/services/pause.service.ts
@@ -0,0 +1,56 @@
+let pauseServiceIsInstantiated: boolean;
+
+interface SavedSettingsState {
+	name: string;
+	instanceService: any;
+	value: any;
+}
+
+class PauseService {
+	settingsServices: SavedSettingsState[] = [];
+
+	constructor() {
+		if (pauseServiceIsInstantiated) {
+			throw new Error('PauseService is already instantiated.');
+		}
+
+		pauseServiceIsInstantiated = true;
+
+		this.settingsServices = [
+			{ name: 'clickFacilite', instanceService: clickFaciliteServiceInstance.setClickFacilite.bind(this), value: '' },
+			{ name: 'colorContrast', instanceService: colorContrastServiceInstance.setColorsContrasts.bind(this), value: '' },
+			{ name: 'cursorAspect', instanceService: cursorAspectServiceInstance.setCursor.bind(this), value: '' },
+			{ name: 'focusAspect', instanceService: focusAspectServiceInstance.setFocus.bind(this), value: '' },
+			{ name: 'fontFamily', instanceService: fontFamilyServiceInstance.setFontFamily.bind(this), value: '' },
+			{ name: 'linkStyle', instanceService: linkStyleServiceInstance.setLinkStyle.bind(this), value: '' },
+			{ name: 'marginAlign', instanceService: marginAlignServiceInstance.setMargin.bind(this), value: '' },
+			{ name: 'readingGuide', instanceService: readingGuideServiceInstance.setReadingMaskGuide.bind(this), value: '' },
+			{ name: 'scroll', instanceService: scrollServiceInstance.setScroll.bind(this), value: '' },
+			{ name: 'textSize', instanceService: textSizeServiceInstance.setFontSize.bind(this), value: '' },
+			{ name: 'textSpacing', instanceService: textSpacingServiceInstance.setSpacingText.bind(this), value: '' }
+		];
+	}
+
+	pauseSettings = (currentSettings: string): void => {
+		const settings: any[] = JSON.parse(currentSettings);
+		settings.forEach((setting: any) => {
+			let settingValues: any = Object.values(setting)[0];
+			this.settingsServices.forEach((settingsService: SavedSettingsState) => {
+				if (settingsService.name === Object.keys(setting)[0]) {
+					settingsService.value = this.getSelectedValue(settingValues);
+				}
+				settingsService.instanceService('noModifications');
+			});
+		});
+	}
+
+	getSelectedValue = (setting: any): string => {
+		return setting.values.split(',')[setting.valueSelected];
+	}
+
+	playSettings = (): void => {
+		this.settingsServices.forEach((settingsService: SavedSettingsState) => {
+			settingsService.instanceService(settingsService.value);
+		});
+	}
+}
