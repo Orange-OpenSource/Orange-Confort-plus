6ac079233587ab6daa1c7330925a4f3d563a5395
Gaël Poupard <g.poupard@groupeonepoint.com>
2024-02-14 16:31:36 +0100

    fix(modale): correction du layout

--- src/app/pages/settings/categories/pointer.component.ts
+++ src/app/pages/settings/categories/pointer.component.ts
@@ -8,7 +8,7 @@ tmplPointer.innerHTML = `
 	</div>
 	<div class="accordion-collapse collapse" id="category-pointer">
 		<div class="accordion-body px-3">
-			<div class="c-category__settings-container d-flex flex-column gap-2">
+			<div class="c-category__settings-container gap-2">
 				<app-cursor-aspect class="c-category__setting" data-can-edit="true"></app-cursor-aspect>
 			</div>
 			<button class="c-category__btn-more btn btn-tertiary mt-3" type="button" data-i18n="moreSettings"></button>
