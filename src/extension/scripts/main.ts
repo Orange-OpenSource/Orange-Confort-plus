0a303a8fb64b9d48e91a96e07aa13d5057da4497
Gaël Poupard <g.poupard@groupeonepoint.com>
2024-03-06 17:01:56 +0100

    feat(persistance)[WIP]: injection de la toolbar au changement d’URL d’un onglet

--- src/extension/scripts/main.ts
+++ src/extension/scripts/main.ts
@@ -66,27 +66,26 @@ chrome.action.onClicked.addListener(async (tab) => {
 	}
 
 	const activations = await chrome.storage.local.get(`${prefix}is-enabled-${tab.id}`);
-	const isLoaded = activations[`${prefix}is-enabled-${tab.id}`];
-	chrome.storage.local.set({[`${prefix}is-enabled-${tab.id}`]: !isLoaded});
+	const isEnabled = activations[`${prefix}is-enabled-${tab.id}`];
+	chrome.storage.local.set({[`${prefix}is-enabled-${tab.id}`]: !isEnabled});
 
 	const injections = await chrome.storage.local.get(`${prefix}is-injected-${tab.id}`);
 	const isInjected = injections[`${prefix}is-injected-${tab.id}`];
 
-	updateButtonIcon(!isLoaded, tab.id as number);
+	updateButtonIcon(!isEnabled, tab.id as number);
 
-	// @note À déporter dans une méthode dédiée (?)
-	if (!isLoaded && !isInjected) {
+	if (!isEnabled && !isInjected) {
 		chrome.scripting.executeScript({
 			target: { tabId: tab.id as number },
 			files: ['js/toolbar.js']
 		});
 		chrome.storage.local.set({[`${prefix}is-injected-${tab.id}`]: true});
-	} else if (!isLoaded && isInjected) {
+	} else if (!isEnabled && isInjected) {
 		chrome.scripting.executeScript({
 			target: { tabId: tab.id as number },
 			files: ['js/restore.js']
 		});
-	} else if (isLoaded && isInjected) {
+	} else if (isEnabled && isInjected) {
 		chrome.scripting.executeScript({
 			target: { tabId: tab.id as number },
 			files: ['js/eject.js']
@@ -100,12 +99,20 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 		chrome.action.disable(tabId);
 	} else {
 		chrome.action.enable(tabId);
-		// @todo Vérifier ici si 'is-opened' et ré-injecter les JS
-		const isOpened = await chrome.storage.local.get(`${prefix}is-opened`);
-		console.log(isOpened[`${prefix}is-opened`]);
-		// @note Les injections JS doivent être déportées dans une méthode dédiée (?)
-		chrome.storage.local.set({[`${prefix}is-enabled-${tabId}`]: false});
-		chrome.storage.local.set({[`${prefix}is-injected-${tabId}`]: false});
+
+		// @todo À tester
+		// @todo À scoper à l’onglet / domaine
+		const openings = await chrome.storage.local.get(`${prefix}is-opened`);
+		const isOpened = openings[`${prefix}is-opened`] === 'true';
+		if (isOpened) {
+			chrome.scripting.executeScript({
+				target: { tabId: tab.id as number },
+				files: ['js/toolbar.js']
+			});
+		}
+
+		chrome.storage.local.set({[`${prefix}is-enabled-${tabId}`]: isOpened});
+		chrome.storage.local.set({[`${prefix}is-injected-${tabId}`]: isOpened});
 	}
 });
 
