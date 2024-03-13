9d0ddd35f4e69af187b88a179c35b8ea45dac698
Thomas DELIERE <t.deliere@groupeonepoint.com>
2024-03-13 16:40:49 +0100

    Rebase suite release v5.0.0-alpha.2

--- dist/extension/js/toolbar.js
+++ dist/extension/js/toolbar.js
@@ -1,5 +1,5 @@
 /*
- * orange-confort-plus - version 5.0.0-alpha.1 - 13/03/2024
+ * orange-confort-plus - version 5.0.0-alpha.2 - 13/03/2024
  * Enhance user experience on web sites
  * © 2014 - 2024 Orange SA
  */
