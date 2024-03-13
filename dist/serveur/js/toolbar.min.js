41bc91909ac2793e90d5181f1bd658f0b442cbfe
Gaël Poupard <g.poupard@groupeonepoint.com>
2024-03-13 16:05:18 +0100

    chore(build): avant version

--- dist/serveur/js/toolbar.min.js
+++ dist/serveur/js/toolbar.min.js
@@ -1,5 +1,5 @@
 /*
- * orange-confort-plus - version 5.0.0-alpha.1 - 06/03/2024
+ * orange-confort-plus - version 5.0.0-alpha.1 - 13/03/2024
  * Enhance user experience on web sites
  * © 2014 - 2024 Orange SA
  */
