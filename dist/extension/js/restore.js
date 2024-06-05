0d7317bf5f117c8b273ab5071e16f5292ac4ee2d
Gaël Poupard <g.poupard@groupeonepoint.com>
2024-06-05 15:30:32 +0200

    fix(extension): éjection et restauration multiples

--- dist/extension/js/restore.js
+++ dist/extension/js/restore.js
@@ -1,9 +1,8 @@
 "use strict";
 // @ts-ignore
-let restore = () => {
+(() => {
     const appRootElt = document.createElement('app-root');
     appRootElt.setAttribute('data-state', 'restored');
     document.body.prepend(appRootElt);
-};
-restore();
+})();
 "EOF";
