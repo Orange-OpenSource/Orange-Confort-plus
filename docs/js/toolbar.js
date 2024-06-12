81471b5a767a383710bc5c608eada521a8acbf59
Gaël Poupard <g.poupard@groupeonepoint.com>
2024-06-12 16:48:51 +0200

    chore(deps): mise à jour des dépendances

--- docs/js/toolbar.js
+++ docs/js/toolbar.js
@@ -1,5 +1,5 @@
 /*
- * orange-confort-plus - version 5.0.0-alpha.4 - 05/06/2024
+ * orange-confort-plus - version 5.0.0-alpha.4 - 12/06/2024
  * Enhance user experience on web sites
  * © 2014 - 2024 Orange SA
  */
