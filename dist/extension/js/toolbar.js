e2c238ce9bcea75a1f3600fe351004dc20cf38fd
Gaël Poupard <g.poupard@groupeonepoint.com>
2024-07-03 17:30:41 +0200

    fix(multi-clic): amélioration du balisage

--- dist/extension/js/toolbar.js
+++ dist/extension/js/toolbar.js
@@ -66,7 +66,7 @@
     function P(a, b, d, f) {
         var c = window.ShadyDOM;
         if (a.shadyDomFastWalk && c && c.inUse) {
-            if (b.nodeType === Node.ELEMENT_NODE && d(b), b.querySelectorAll) for (a = c.nativeMethods.querySelectorAll.call(b, "*"), 
+            if (b.nodeType === Node.ELEMENT_NODE && d(b), b.querySelectorAll) for (a = c.nativeMethods.querySelectorAll.call(b, "*"),
             b = 0; b < a.length; b++) d(a[b]);
         } else M(b, d, f);
     }
@@ -205,7 +205,7 @@
             if (e.localName !== d) throw Error("Failed to construct '" + d + "': The constructed element's local name is incorrect.");
             return e;
         } catch (g) {
-            return X(g), b = null === f ? n.call(b, d) : p.call(b, f, d), Object.setPrototypeOf(b, HTMLUnknownElement.prototype), 
+            return X(g), b = null === f ? n.call(b, d) : p.call(b, f, d), Object.setPrototypeOf(b, HTMLUnknownElement.prototype),
             b.__CE_state = 2, b.__CE_definition = void 0, R(a, b), b;
         }
         b = null === f ? n.call(b, d) : p.call(b, f, d);
@@ -214,7 +214,7 @@
     }
     function X(a) {
         var b = "", d = "", f = 0, c = 0;
-        a instanceof Error ? (b = a.message, d = a.sourceURL || a.fileName || "", f = a.line || a.lineNumber || 0, 
+        a instanceof Error ? (b = a.message, d = a.sourceURL || a.fileName || "", f = a.line || a.lineNumber || 0,
         c = a.column || a.columnNumber || 0) : b = "Uncaught " + String(a);
         var e = void 0;
         void 0 === ErrorEvent.prototype.initErrorEvent ? e = new ErrorEvent("error", {
@@ -224,7 +224,7 @@
             lineno: f,
             colno: c,
             error: a
-        }) : (e = document.createEvent("ErrorEvent"), e.initErrorEvent("error", !1, !0, b, d, f), 
+        }) : (e = document.createEvent("ErrorEvent"), e.initErrorEvent("error", !1, !0, b, d, f),
         e.preventDefault = function() {
             Object.defineProperty(this, "defaultPrevented", {
                 configurable: !0,
@@ -261,7 +261,7 @@
         this.h = a;
         this.g = b;
         V(this.h, this.g);
-        "loading" === this.g.readyState && (this.l = new MutationObserver(this.G.bind(this)), 
+        "loading" === this.g.readyState && (this.l = new MutationObserver(this.G.bind(this)),
         this.l.observe(this.g, {
             childList: !0,
             subtree: !0
@@ -525,8 +525,8 @@
                     var l = this.previousSibling;
                     e.call(this, h, k);
                     g(l || this.parentNode.firstChild, this);
-                } else if ("afterbegin" === h) l = this.firstChild, e.call(this, h, k), g(this.firstChild, l); else if ("beforeend" === h) l = this.lastChild, 
-                e.call(this, h, k), g(l || this.firstChild, null); else if ("afterend" === h) l = this.nextSibling, 
+                } else if ("afterbegin" === h) l = this.firstChild, e.call(this, h, k), g(this.firstChild, l); else if ("beforeend" === h) l = this.lastChild,
+                e.call(this, h, k), g(l || this.firstChild, null); else if ("afterend" === h) l = this.nextSibling,
                 e.call(this, h, k), g(this.nextSibling, l); else throw new SyntaxError("The value provided (" + String(h) + ") is not one of 'beforebegin', 'afterbegin', 'beforeend', or 'afterend'.");
             };
         }
@@ -601,7 +601,7 @@
             var f = document.__CE_registry.C.get(d);
             if (!f) throw Error("Failed to construct a custom element: The constructor was not registered with `customElements`.");
             var c = f.constructionStack;
-            if (0 === c.length) return c = n.call(document, f.localName), Object.setPrototypeOf(c, d.prototype), 
+            if (0 === c.length) return c = n.call(document, f.localName), Object.setPrototypeOf(c, d.prototype),
             c.__CE_state = 1, c.__CE_definition = f, R(a, c), c;
             var e = c.length - 1, g = c[e];
             if (g === Fa) throw Error("Failed to construct '" + f.localName + "': This element was already constructed.");
@@ -3942,7 +3942,7 @@ customElements.define("app-btn-modal", BtnModalComponent);
 
 const btnSettingLayout = document.createElement("template");
 
-btnSettingLayout.innerHTML = `\n\t<button type="button" class="sc-btn-setting btn btn-primary flex-column justify-content-between w-100 px-1">\n\t\t<div class="d-flex flex-column">\n\t\t\t<span></span>\n\t\t\t<app-icon data-size="1.5em"></app-icon>\n\t\t</div>\n\t\t<ul class="d-flex gap-1 align-items-center mt-2 mb-0 list-unstyled"></ul>\n\t</button>\n`;
+btnSettingLayout.innerHTML = `\n\t<button type="button" class="sc-btn-setting btn btn-primary flex-column justify-content-between w-100 px-1">\n\t\t<span class="d-flex flex-column">\n\t\t\t<span></span>\n\t\t\t<app-icon data-size="1.5em"></app-icon>\n\t\t</span>\n\t\t<span class="sc-btn-setting__values d-flex gap-1 align-items-center mt-2 mb-0"></span>\n\t\t\x3c!-- @todo Nom accessible : réglage, valeur actuelle 1/n, changer en 2/n --\x3e\n\t\t\x3c!-- @note Exemple : « Police Accessible-DfA 1/3, changer en Arial 2/3 » --\x3e\n\t\t\x3c!-- @note Si on peut associer un libellé a une valeur c’est cool --\x3e\n\t</button>\n`;
 
 class BtnSettingComponent extends HTMLElement {
     static observedAttributes=[ "data-values", "data-active-value", "data-name", "data-disabled" ];
@@ -3964,7 +3964,7 @@ class BtnSettingComponent extends HTMLElement {
     }
     connectedCallback() {
         this.settingBtn = this.querySelector("button");
-        this.btnContentSlots = this.querySelector("ul");
+        this.btnContentSlots = this.querySelector(".sc-btn-setting__values");
         this.settingBtn.addEventListener("click", this.handler);
         this.setDisabledState();
     }
