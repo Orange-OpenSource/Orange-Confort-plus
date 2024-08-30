11e7e44f8a8501858acb1f3a806074e90ffaa5b3
Thomas DELIERE <t.deliere@groupeonepoint.com>
2024-08-30 15:11:47 +0200

    Test thomas colorisation de texte.

--- src/app/services/settings/abstracts/body-selector.service.ts
+++ src/app/services/settings/abstracts/body-selector.service.ts
@@ -2,4 +2,49 @@ abstract class BodySelectorService {
 	getBodyElements(): NodeListOf<Element> {
 		return document.body.querySelectorAll(':not(script):not(app-root)');
 	}
+
+	getTextNodes(element: Node): Node[] {
+		const textNodes: Node[] = [];
+		const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT);
+
+		while (walker.nextNode()) {
+			textNodes.push(walker.currentNode);
+		}
+
+		return textNodes;
+	}
+
+	isAlreadyEdited(node: Node, className: string): boolean {
+		return node.parentNode instanceof HTMLElement && node.parentNode.classList.contains(className);
+	}
+
+	resetToDefaultBody = (classToDelete: string[]): void => {
+		const spans = document.querySelectorAll(classToDelete.map(c => `.${c}`).join(', '));
+
+		spans.forEach(span => {
+			const textNode = document.createTextNode(span.textContent);
+			span.replaceWith(textNode);
+		});
+
+		const body = document.body;
+		this.concatTextNodes(body);
+	}
+
+	concatTextNodes = (element: any): void => {
+		let child = element.firstChild;
+		while (child) {
+			if (child.nodeType === Node.ELEMENT_NODE) {
+				this.concatTextNodes(child);
+			}
+
+			if (child.nodeType === Node.TEXT_NODE) {
+				while (child.nextSibling && child.nextSibling.nodeType === Node.TEXT_NODE) {
+					child.textContent += child.nextSibling.textContent;
+					child.parentNode.removeChild(child.nextSibling);
+				}
+			}
+
+			child = child.nextSibling;
+		}
+	}
 }
