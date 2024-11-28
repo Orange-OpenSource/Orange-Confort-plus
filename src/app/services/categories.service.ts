a796b0bfb6c767c2e57c38a9bde86ea5327a8744
Thomas DELIERE <t.deliere@groupeonepoint.com>
2024-11-28 16:30:52 +0100

    Garde sauvegardé la catégorie ouverte selon le monde d'usage.

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -23,33 +23,46 @@ class CategoriesService {
 		categoriesServiceIsInstantiated = true;
 	}
 
-	openCategory = (category: string, open: boolean): void => {
+	openCategory = (category: string): void => {
+		let currentCategory = 'allClosed';
 		const mainIndex = this.settingAccordions.findIndex(o => o.name === category.toLowerCase());
 		this.settingAccordions.forEach((accordion: AccordionState, index) => {
 			accordion.open = index === mainIndex ? !accordion.open : false;
+			if (accordion.open) {
+				currentCategory = accordion.name;
+			}
 		});
+
+		localStorageServiceInstance.setItem('current-category', currentCategory);
 	}
 
-	openMainCategory = (selectedMode: string): void => {
+	openMainCategory = (selectedMode: string): Promise<AccordionState[]> => {
 		let mainAccordion: string;
+		return localStorageServiceInstance.getItem('current-category').then((result: any) => {
+			if (result) {
+				mainAccordion = result;
+			} else {
+				if (this.selectedMode !== selectedMode) {
+					this.selectedMode = selectedMode;
 
-		if (this.selectedMode !== selectedMode) {
-			this.selectedMode = selectedMode;
-
-			switch (selectedMode) {
-				case 'visionPlus':
-					mainAccordion = 'app-layout';
-					break;
-				case 'facilePlus':
-				default:
-					mainAccordion = 'app-text';
-					break;
+					switch (selectedMode) {
+						case 'visionPlus':
+							mainAccordion = 'app-layout';
+							break;
+						case 'facilePlus':
+						default:
+							mainAccordion = 'app-text';
+							break;
+					}
+				}
 			}
 
 			this.settingAccordions.forEach((accordion: AccordionState, index) => {
 				accordion.open = accordion.name === mainAccordion ? true : false;
 			});
-		}
+
+			return this.settingAccordions;
+		});
 	}
 }
 
