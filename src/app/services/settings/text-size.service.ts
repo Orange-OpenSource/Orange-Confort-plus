dd194cb24f5602e8b4c55b6eee7dfc65591d940c
b.ingoglia <b.ingoglia@groupeonepoint.com>
2025-03-04 14:57:07 +0100

    feat(text-size-service): wording & text size setting

--- src/app/services/settings/text-size.service.ts
+++ src/app/services/settings/text-size.service.ts
@@ -11,10 +11,16 @@ class TextSizeService {
 	}
 
 	setFontSize = (value: string): void => {
+		const nbValue = Number(value);
+
 		const fontSize = value === DEFAULT_VALUE ? null : `${value}%`;
 		document.documentElement.style.fontSize = fontSize;
 
-		const layoutState = (Number(value) >= 350) ? 'active' : DEFAULT_VALUE;
+
+		const lineHeight = !isNaN(nbValue) && (nbValue >= 130) ? `${0.75 * nbValue / 100}` : DEFAULT_VALUE;
+		document.documentElement.style.lineHeight = lineHeight;
+
+		const layoutState = (nbValue >= 200) ? 'active' : DEFAULT_VALUE;
 		deleteLayoutServiceInstance.setDeleteLayout(layoutState);
 	}
 }
