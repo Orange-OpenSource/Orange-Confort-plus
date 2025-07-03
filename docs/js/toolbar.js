408582352366faecf2c73b9640b2fc6320125130
Gaël Poupard <g.poupard@groupeonepoint.com>
2025-07-03 09:34:22 +0200

    5.0.0-beta.6

--- docs/js/toolbar.js
+++ docs/js/toolbar.js
@@ -1,5 +1,5 @@
 /*
- * orange-confort-plus - version 5.0.0-beta.5 - 02/07/2025
+ * orange-confort-plus - version 5.0.0-beta.6 - 03/07/2025
  * Enhance user experience on web sites
  * © 2014 - 2025 Orange SA
  */
@@ -57,7 +57,7 @@ const TEXT_ALTERNATE_LINES = `${PREFIX}alternateLines`;
 
 const BODY_ELEMENTS_FILTER = "script,style,link,meta";
 
-VERSION = "5.0.0-beta.5";
+VERSION = "5.0.0-beta.6";
 
 "use strict";
 
