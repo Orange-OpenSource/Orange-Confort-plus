49a8eba2c0bd2cef62505425140a2df5b06eca2a
Gaël Poupard <g.poupard@groupeonepoint.com>
2025-07-17 15:35:08 +0200

    docs(site): typo dans la version anglaise

--- dist/extension/js/toolbar.js
+++ dist/extension/js/toolbar.js
@@ -1,5 +1,5 @@
 /*
- * orange-confort-plus - version 5.0.0-beta.9 - 16/07/2025
+ * orange-confort-plus - version 5.0.0-beta.9 - 17/07/2025
  * Enhance user experience on web sites
  * © 2014 - 2025 Orange SA
  */
