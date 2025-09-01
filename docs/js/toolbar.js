0549eef212bdb039cfec36e220a9cfdf5e1a9ad0
b.ingoglia <b.ingoglia@groupeonepoint.com>
2025-09-01 16:00:54 +0200

    feat: translations

--- docs/js/toolbar.js
+++ docs/js/toolbar.js
@@ -1,5 +1,5 @@
 /*
- * orange-confort-plus - version 5.0.0-beta.9 - 25/08/2025
+ * orange-confort-plus - version 5.0.0-beta.9 - 01/09/2025
  * Enhance user experience on web sites
  * © 2014 - 2025 Orange SA
  */
