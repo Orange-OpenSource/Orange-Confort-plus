23b4b2f8954cf464b6e6e8e560cb97dca36bcf8a
gildaB <gildas1.bescond@orange.com>
2025-12-17 11:45:52 +0100

    fix:merge

--- dist/extension/js/toolbar.js
+++ dist/extension/js/toolbar.js
@@ -1,5 +1,5 @@
 /*
- * orange-confort-plus - version 5.2.0 - 02/12/2025
+ * orange-confort-plus - version 5.2.0 - 17/12/2025
  * Enhance user experience on web sites
  * © 2014 - 2025 Orange SA
  */
